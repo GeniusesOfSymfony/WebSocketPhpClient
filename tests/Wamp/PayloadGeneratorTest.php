967252f15c303a272c12975bf94cfcdf8743a6d3
Michael Babker <michael.babker@gmail.com>
2020-03-12 16:18:20 -0500

    Basic tests

--- tests/Wamp/PayloadGeneratorTest.php
+++ tests/Wamp/PayloadGeneratorTest.php
@@ -3,10 +3,32 @@
 namespace Gos\Component\WebSocketClient\Tests\Wamp;
 
 use Gos\Component\WebSocketClient\Wamp\PayloadGenerator;
+use Gos\Component\WebSocketClient\Wamp\Protocol;
+use Gos\Component\WebSocketClient\Wamp\WebsocketPayload;
 use PHPUnit\Framework\TestCase;
 
 final class PayloadGeneratorTest extends TestCase
 {
+    public function testAnUnmaskedPayloadIsEncodedForAPrefixMessageAsATextOpcode(): void
+    {
+        $payload = (new WebsocketPayload())
+            ->setOpcode(WebsocketPayload::OPCODE_TEXT)
+            ->setMask(0x0)
+            ->setPayload(json_encode([Protocol::MSG_PREFIX, 'test', 'http://localhost/test']));
+
+        $this->assertNotEmpty((new PayloadGenerator())->encode($payload));
+    }
+
+    public function testAMaskedPayloadIsEncodedForAPrefixMessageAsATextOpcode(): void
+    {
+        $payload = (new WebsocketPayload())
+            ->setOpcode(WebsocketPayload::OPCODE_TEXT)
+            ->setMask(0x1)
+            ->setPayload(json_encode([Protocol::MSG_PREFIX, 'test', 'http://localhost/test']));
+
+        $this->assertNotEmpty((new PayloadGenerator())->encode($payload));
+    }
+
     public function testAClosePayloadIsGenerated(): void
     {
         $this->assertStringEndsWith('ttfn', (new PayloadGenerator())->generateClosePayload());
