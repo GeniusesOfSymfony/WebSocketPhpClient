4d314b04d45de71103a739c3c3b0210a16e59c32
Michael Babker <michael.babker@gmail.com>
2020-03-13 11:20:41 -0500

    Test the generator with payloads of different lengths

--- tests/Wamp/PayloadGeneratorTest.php
+++ tests/Wamp/PayloadGeneratorTest.php
@@ -29,6 +29,46 @@ final class PayloadGeneratorTest extends TestCase
         $this->assertNotEmpty((new PayloadGenerator())->encode($payload));
     }
 
+    public function testAnUnmaskedPayloadIsEncodedForACallMessageWithAMediumPayloadAsATextOpcode(): void
+    {
+        $payload = (new WebsocketPayload())
+            ->setOpcode(WebsocketPayload::OPCODE_TEXT)
+            ->setMask(0x0)
+            ->setPayload(json_encode([Protocol::MSG_CALL, uniqid('', true), '/test', str_repeat('a', 0xc8)]));
+
+        $this->assertNotEmpty((new PayloadGenerator())->encode($payload));
+    }
+
+    public function testAMaskedPayloadIsEncodedForACallMessageWithAMediumPayloadAsATextOpcode(): void
+    {
+        $payload = (new WebsocketPayload())
+            ->setOpcode(WebsocketPayload::OPCODE_TEXT)
+            ->setMask(0x1)
+            ->setPayload(json_encode([Protocol::MSG_CALL, uniqid('', true), '/test', str_repeat('a', 0xc8)]));
+
+        $this->assertNotEmpty((new PayloadGenerator())->encode($payload));
+    }
+
+    public function testAnUnmaskedPayloadIsEncodedForACallMessageWithALongPayloadAsATextOpcode(): void
+    {
+        $payload = (new WebsocketPayload())
+            ->setOpcode(WebsocketPayload::OPCODE_TEXT)
+            ->setMask(0x0)
+            ->setPayload(json_encode([Protocol::MSG_CALL, uniqid('', true), '/test', str_repeat('a', 0x10000)]));
+
+        $this->assertNotEmpty((new PayloadGenerator())->encode($payload));
+    }
+
+    public function testAMaskedPayloadIsEncodedForACallMessageWithALongPayloadAsATextOpcode(): void
+    {
+        $payload = (new WebsocketPayload())
+            ->setOpcode(WebsocketPayload::OPCODE_TEXT)
+            ->setMask(0x1)
+            ->setPayload(json_encode([Protocol::MSG_CALL, uniqid('', true), '/test', str_repeat('a', 0x10000)]));
+
+        $this->assertNotEmpty((new PayloadGenerator())->encode($payload));
+    }
+
     public function testAClosePayloadIsGenerated(): void
     {
         $this->assertStringEndsWith('ttfn', (new PayloadGenerator())->generateClosePayload());
